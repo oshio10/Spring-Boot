25e7013ba5625dbc772f269d50b9bc847dc83c29
oshio10 <patricksunchen@gmail.com>
2025-02-13 09:36:10 +0100

    .

---
+++ src/main/java/fr/devavance/tp_spring_mvc_jpa/TpSpringMvcJpaApplication.java
@@ -0,0 +1,13 @@
+package fr.devavance.tp_spring_mvc_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringMvcJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringMvcJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/devavance/tp_spring_mvc_jpa/TpSpringMvcJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.devavance.tp_spring_mvc_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringMvcJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
